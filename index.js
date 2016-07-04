14143137ed8af774b7cc381f7a42aad3d5637476
Dan Finlay <dan@danfinlay.com>
2016-07-04 11:49:40 -0700

    Fix voice playing

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var Discord = require("discord.js");
 var auth = require('./auth.json')
 
+var path = require('path')
+var friendsPath = path.join(__dirname, 'friends_forever.mp3')
 var friendsForever = 'https://www.youtube.com/watch?v=MwreUpE1Ulk'
+
+var channels
 
 // Playing random game
 var games = [
@@ -30,7 +34,12 @@ mybot.on('ready', function(err) {
     if (message.content[0] !== '!') { return }
 
     if (message.content === '!friends') {
-      mybot.reply(message, friendsForever)
+      //mybot.reply(message, friendsForever)
+      channels = message.channel.server.channels
+
+      singThatSong(mybot)
+
+    } else if (message.content === '!sing') {
     }
 
     if (message.content === '!ping') {
@@ -39,3 +48,31 @@ mybot.on('ready', function(err) {
   })
 })
 
+function singThatSong(mybot) {
+  var channel = channels.filter((channel) => {
+    return channel.name === 'General'
+  })[0]
+
+  mybot.joinVoiceChannel(channel, function(err, voiceConnection) {
+
+    if (!voiceConnection.playing) {
+
+      voiceConnection.playFile(friendsPath, {
+        volume: 0.25
+      }, function(err, streamIntent) {
+        if (err) {
+          console.error('Playing friends failed')
+          console.error(err)
+        }
+
+        streamIntent.on('end', function() {
+          mybot.leaveVoiceChannel(channel, function(err) {
+            if (err) {
+              console.log('Bot failed to leave channel')
+            }
+          })
+        })
+      })
+    }
+  })
+}
